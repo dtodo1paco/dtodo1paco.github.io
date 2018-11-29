d86d87b2fa1c449db0e22a3ba396cea3953941b1
dtodo1paco <dtodo1paco@gmail.com>
2018-11-29 13:28:50 +0100

    autogenerated for production mode

---
+++ cv/build/precache-manifest.bc7d89c3a5689b6574415bc19b3ea8e0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5129b60597a8c3b1d98d1d8aa09c8469",
+    "url": "/cv/static/media/dtodo1paco.5129b605.png"
+  },
+  {
+    "revision": "683c2b7d493176fa56632cd8d33d8d10",
+    "url": "/cv/static/media/MOrSA.683c2b7d.jpg"
+  },
+  {
+    "revision": "88302b9f7fd0ef21f639fd75f670e4af",
+    "url": "/cv/static/media/CVdemo.88302b9f.gif"
+  },
+  {
+    "revision": "084ac45aee39707f976e",
+    "url": "/cv/static/js/runtime~main.084ac45a.js"
+  },
+  {
+    "revision": "85c188b4bc817b42bb09",
+    "url": "/cv/static/js/main.85c188b4.chunk.js"
+  },
+  {
+    "revision": "a92767f2101e0cf712db",
+    "url": "/cv/static/js/1.a92767f2.chunk.js"
+  },
+  {
+    "revision": "85c188b4bc817b42bb09",
+    "url": "/cv/static/css/main.07ed4bbd.chunk.css"
+  },
+  {
+    "revision": "a92767f2101e0cf712db",
+    "url": "/cv/static/css/1.3db88c6b.chunk.css"
+  },
+  {
+    "revision": "20e65da32a61482379637be8637f8a7f",
+    "url": "/cv/index.html"
+  }
+];
